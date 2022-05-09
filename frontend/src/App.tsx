edae1bb5d4c7bbbe17d26135a6466c6939b481d3
Vinicius 'Davina' Ferreira <viniciusferreira0708@gmail.com>
2022-05-09 12:32:07 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
-import Navbar from "./Components/Navbar";
-
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'Components/pages/Listing';
+import Form from 'Components/pages/Form';
+import Navbar from "Components/Navbar";
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+

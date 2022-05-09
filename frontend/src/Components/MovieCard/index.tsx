b4a538432bfcd2e55ab0e759ab755abd40d4d6ca
Vinicius 'Davina' Ferreira <viniciusferreira0708@gmail.com>
2022-05-09 13:25:13 -0300

    Navigation buttons

--- frontend/src/Components/MovieCard/index.tsx
+++ frontend/src/Components/MovieCard/index.tsx
@@ -1,4 +1,5 @@
 import MovieScore from "Components/MovieScore";
+import { Link } from "react-router-dom";
 const movie = {
     id: 1,
     image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
@@ -15,7 +16,9 @@ return (
     <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
+        <Link to={`/form/${movie.id}`}>
         <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
     </div>
 </div>);
 }
